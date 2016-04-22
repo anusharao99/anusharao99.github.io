c37a6eb453a89a70bd0c68e497a0cd2e0aaef3f5
Digital Media Student <digital@browardtech.local>
2016-04-22 14:19:54 -0400

    I am a stupid dumb

---
+++ animation/newproj_edge.js
@@ -0,0 +1,39 @@
+
+(function(compId){var _=null,y=true,n=false,or='-webkit-transform-origin',rz='rotateZ',x2='5.0.0',x4='rgba(0,0,0,0)',x15='235px',g='image',x22='51px',e19='${_444px-Frame2svg}',x24='0',m='rect',x13='0px',x23='GooglePlay-Color',i='none',x3='6.0.0.400',on='msTransformOrigin',oo='-o-transform-origin',x29='52px',o='opacity',e27='${GooglePlay-Color}',x26='true',x14='193px',x16='auto',x17='_444px-Frame2svg',x21='128px',x1='6.0.0',ot='transform-origin',x11='rgba(255,255,255,1)',x28='appstore_color',om='-ms-transform-origin',oz='-moz-transform-origin',e12='${BACKGROUND-02}',x20='@@0@@% @@1@@%';var g25='GooglePlay-Color.png',g6='BACKGROUND-02.jpg',g7='DWNLD.png',g30='rtrt.png',g8='v1-logo2.png',g5='BACKGROUND-01.jpg',g10='dgmt2.png',g9='GooglePlay-Grey.png',g18='444px-Frame2.svg.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'BACKGROUND-01',t:g,r:['0px','0px','300px','600px','auto','auto'],f:[x4,im+g5,'0px','0px']},{id:'BACKGROUND-02',t:g,r:['0px','0px','300px','600px','auto','auto'],o:'1',f:[x4,im+g6,'0px','0px'],filter:[0,-31,0.96666666666667,1.3333336666666,0,0,0,0,"rgba(0,0,0,0)",0,0,0]},{id:'DWNLD',t:g,r:['28px','289px','244px','383px','auto','auto'],f:[x4,im+g7,'0px','0px']},{id:'ThePainting',symbolName:'ThePainting',t:m,r:['53','182','193','235','auto','auto']},{id:'v1-logo2',t:g,r:['37px','34px','226px','78px','auto','auto'],f:[x4,im+g8,'0px','0px']},{id:'GooglePlay-Grey',t:g,r:['161px','539px','128px','51px','auto','auto'],cu:'pointer',f:[x4,im+g9,'0px','0px']},{id:'GooglePlay_ButtonColor',symbolName:'GooglePlay_ButtonColor',t:m,r:['161','539','128','51','auto','auto'],cu:'pointer'},{id:'apstore_blank',t:g,r:['11px','539','128px','52px','8446px','3547px'],cu:'pointer',f:[x4,im+g10,'0px','0px']},{id:'appstore_color',symbolName:'Symbol_1',t:m,r:['12px','539px','128','52','auto','auto']}],style:{'${Stage}':{isStage:true,r:['null','null','300px','600px','auto','auto'],overflow:'hidden',f:[x11]}}},tt:{d:1000,a:y,data:[["eid5",o,0,500,"linear",e12,'1','0'],["eid6",o,500,500,"linear",e12,'0.000000','1'],["eid7",o,1000,0,"linear",e12,'1','1']]}},"ThePainting":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x13,x13,x14,x15,x16,x16],id:x17,t:g,f:[x4,im+g18,x13,x13]}],style:{'${symbolSelector}':{r:[_,_,x14,x15]}}},tt:{d:500,a:n,data:[["eid60",or,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid257",oz,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid258",om,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid259",on,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid260",oo,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid261",ot,0,0,"linear",e19,[50,18],[50,18],{vt:x20}],["eid61",rz,0,500,"linear",e19,'0deg','25deg']]}},"GooglePlay_ButtonColor":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x13,x13,x21,x22,x16,x16],id:x23,o:x24,t:g,f:[x4,im+g25,x13,x13]}],style:{'${symbolSelector}':{isStage:x26,r:[undefined,undefined,x21,x22]}}},tt:{d:250,a:n,data:[["eid80",o,0,250,"linear",e27,'0.000000','1']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x28,t:g,r:[x13,x13,x21,x29,x16,x16],f:[x4,im+g30,x13,x13]}],style:{'${symbolSelector}':{isStage:x26,r:[undefined,undefined,x21,x29]}}},tt:{d:0,a:n,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-531669617");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ThePainting}","mouseover",function(sym,e){sym.getSymbol("ThePainting").play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ThePainting}","mouseout",function(sym,e){sym.getSymbol("ThePainting").playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${GooglePlay_ButtonColor}","mouseover",function(sym,e){sym.getSymbol("GooglePlay_ButtonColor").play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${GooglePlay_ButtonColor}","mouseout",function(sym,e){sym.getSymbol("GooglePlay_ButtonColor").playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${appstore_color}","mouseover",function(sym,e){sym.getSymbol("appstore_color").play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${appstore_color}","mouseout",function(sym,e){sym.getSymbol("appstore_color").play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'ThePainting'
+(function(symbolName){})("ThePainting");
+//Edge symbol end:'ThePainting'
+
+//=========================================================
+
+//Edge symbol: 'GooglePlay_ButtonColor'
+(function(symbolName){})("GooglePlay_ButtonColor");
+//Edge symbol end:'GooglePlay_ButtonColor'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-531669617");
